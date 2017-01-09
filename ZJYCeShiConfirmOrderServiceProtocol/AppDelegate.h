bcfb6c74780d2eadaff7a25ec021b546d4a98167
张俊飞 <zhangjunfei@zhujia360.com>
2017-01-09 15:31:25 +0800

    Initial Commit

---
+++ ZJYCeShiConfirmOrderServiceProtocol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJYCeShiConfirmOrderServiceProtocol
+//
+//  Created by apple on 2017/1/9.
+//  Copyright © 2017年 JunFly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
